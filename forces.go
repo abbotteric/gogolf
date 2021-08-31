958ad4daed8c69cd50914d49861ee76017aea946
Eric Abbott <eric@safecorp.com>
2021-08-30 23:57:03 -0700

    added drag force

--- forces.go
+++ forces.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"math"
+)
+
 /*
 types
 */
@@ -33,9 +37,30 @@ var impact = Vector{22.464, 9.076}
 func step(b Ball, dt float64, impact_force Vector) Ball {
 	var v_new Vector
 	var f_calc = impact_force
+	var m_v = math.Sqrt(math.Pow(b.vel.x, 2) * math.Pow(b.vel.y, 2)) // magnitude of the velocity
 
 	// gravity
 	f_calc.y += M_gb * G
+
+	// drag
+	if m_v != 0 {
+		// magnitude of the drag
+		var m_drag = 0.5 * rho * m_v * C_d * (math.Pi * R_gb * R_gb)
+		var f_drag_x = m_drag * math.Sin(math.Atan(b.vel.x/b.vel.y))
+		var f_drag_y = m_drag * math.Cos(math.Atan(b.vel.x/b.vel.y))
+
+		// make sure the drag force and the velocity are in opposite directions
+		if f_drag_x*b.vel.x > 0 {
+			f_drag_x = -1 * f_drag_x
+		}
+		if f_drag_y*b.vel.y > 0 {
+			f_drag_y = -1 * f_drag_y
+		}
+
+		// add the drag forces to the ball flight
+		f_calc.x += f_drag_x
+		f_calc.y += f_drag_y
+	}
 
 	// velocity calculation
 	v_new.x = b.vel.x + (f_calc.x/M_gb)*dt
