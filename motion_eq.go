f7a161e49d9b3215de3f602efd5ee8ba4eaa6ee4
Eric Abbott <eric@safecorp.com>
2021-08-31 00:02:02 -0700

    helper function cleanup

--- motion_eq.go
+++ motion_eq.go
@@ -30,27 +30,11 @@ type worldSize struct {
 /*
 util functions
 */
-// func pos_at_t(t float64, dv float64, iv float64, ic float64) float64 {
-// 	return 0.5*dv*t*t + iv*t + ic
-// }
-
 func real_space_to_img_space(p worldPoint, s worldSize, img image.RGBA) image.Point {
 	return image.Point{int(math.Round(float64(img.Rect.Max.X) * (p.x / s.width))), img.Rect.Max.Y - int(math.Round(float64(img.Rect.Max.Y)*(p.y/s.height)))}
 }
 
-/*
-	Set up some constants
-*/
-const dt = 0.1
-const G = -9.8
-const scale = 10.0
-
-var world = worldSize{200, 50}
-
-/*
-main
-*/
-func main() {
+func generate_image_file(world worldSize) *image.RGBA {
 	width := int(math.Round(world.width * scale))
 	height := int(math.Round(world.height * scale))
 
@@ -60,13 +44,29 @@ func main() {
 	img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
 
 	black := color.Black
-	white := color.White
 
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
 			img.Set(x, y, black)
 		}
 	}
+
+	return img
+}
+
+/*
+	Set up some constants
+*/
+const dt = 0.1
+const G = -9.8
+const scale = 10.0
+
+/*
+main
+*/
+func main() {
+	var world = worldSize{200, 50}
+	var img = generate_image_file(world)
 
 	t := 0.0
 
@@ -81,7 +81,7 @@ func main() {
 		}
 
 		imgSpace := real_space_to_img_space(worldPoint(nb.pos), world, *img)
-		img.Set(imgSpace.X, imgSpace.Y, white)
+		img.Set(imgSpace.X, imgSpace.Y, color.White)
 
 		fmt.Printf("%f || %f, %f || %f, %f || %d, %d\n", t, nb.pos.x, nb.pos.y, nb.vel.x, nb.vel.y, imgSpace.X, imgSpace.Y)
 
